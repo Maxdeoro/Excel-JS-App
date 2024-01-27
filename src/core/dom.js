28d79de00dc4479015bebafc57b7ba291310e5ed
Maxdeoro <maxdeoro@gmail.com>
2024-01-27 23:48:22 +0300

    one cell selection

--- src/core/dom.js
+++ src/core/dom.js
@@ -25,6 +25,10 @@ class Dom {
         this.$el.removeEventListener(eventType, callback);
     };
 
+    find(selector) {
+        return $(this.$el.querySelector(selector));
+    };
+
     append(node) {
         if (node instanceof Dom) {
             node = node.$el;
@@ -57,9 +61,15 @@ class Dom {
     css(styles={}) {
         Object.keys(styles).forEach((key) => {
             this.$el.style[key] = styles[key];
-            // console.log(key);
-            // console.log(styles[key]);
         });
+    };
+
+    addClass(className) {
+        this.$el.classList.add(className);
+    };
+
+    removeClass(className) {
+        this.$el.classList.remove(className);
     };
 }
 
