7039ef6a470787e09e25e0338475df9326971fd9
Maxdeoro <maxdeoro@gmail.com>
2024-02-05 22:59:03 +0300

    creating Store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { Toolbar } from './components/toolbar/Toolbar';
 import { Formula } from './components/formula/Formula';
 import { Table } from './components/table/Table';
 import './scss/index.scss';
+import { createStore } from './core/createStore';
+import { rootReducer } from './myRedux/rootReduser';
 
-// console.log('It is working!');
+const store = createStore(rootReducer);
 
 const excel = new Excel('#app', {
     components: [Header, Toolbar, Formula, Table],
+    store,
 });
 
 excel.render();
 
-console.log('Excel: ', excel);
+// console.log('Excel: ', excel);
