d165335e4536b06bac783e5c0cc4771a672ca617
Maxdeoro <maxdeoro@gmail.com>
2024-02-08 23:29:53 +0300

    save state for Cells

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,24 +4,34 @@ const CODES = {
     Z: 90           // 'Z'.charCodeAt()
 };
 
-// function toCell(_, col) {
-//    return `<div class="cell" contenteditable data-col='${col}'></div>`;
-// };
-function toCell(row) {
+const DEFAULT_WIDTH = 120;
+
+function getWidth(state, index) {
+    return (state[index] || DEFAULT_WIDTH) + 'px';
+};
+
+function toCell(state, row) {
     return function(_, col) {
+        const width = getWidth(state.colState, col);
         return `<div 
                 class='cell' 
                 contenteditable 
                 data-col='${col}' 
                 data-id='${row}:${col}' 
-                data-type='cell'
+                data-type='cell' 
+                style='width: ${width}'
                 >
                 </div>`;
     };
 };
 
-function toColumn(col, index) {
-    return `<div class='column' data-type='resizable' data-col='${index}'>
+function toColumn({col, index, width}) {
+    return `<div 
+                class='column' 
+                data-type='resizable' 
+                data-col='${index}' 
+                style='width: ${width}'
+                >
                 ${col}
                 <div class='col-resize' data-resize='col'></div>
             </div>`;
@@ -44,12 +54,23 @@ function toChar(_, index) {
     return String.fromCharCode(CODES.A + index);
 };
 
-export function createTable(rowsCount=15) {
+function withWidthFrom(state) {
+    return function(col, index) {
+        return {
+            col,
+            index,
+            width: getWidth(state.colState, index)
+        };
+    };
+};
+
+export function createTable(rowsCount=15, state={}) {
     const colsCount = CODES.Z - CODES.A + 1;
     const rows = [];
     const cols = new Array(colsCount)
         .fill('')
         .map(toChar)
+        .map(withWidthFrom(state))
         .map(toColumn)
         .join('');
 
@@ -58,8 +79,7 @@ export function createTable(rowsCount=15) {
     for (let row = 0; row < rowsCount; row++) {
         const cells = new Array(colsCount)
             .fill('')
-            // .map(toCell)
-            .map(toCell(row))
+            .map(toCell(state, row))
             .join('');
 
         rows.push(createRow(row + 1, cells));
